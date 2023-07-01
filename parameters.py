ba95cad0ed627359003974120af7e370e756b497
NNrico <100316667+NNrico@users.noreply.github.com>
2023-07-01 13:54:59 +0200

    Add files via upload

--- parameters.py
+++ parameters.py
@@ -27,13 +27,15 @@ features_from_class = {
     3: ['11']
 }
 
-# plt.figure(0)
-# plt.title("pricing_probabilities")
-# plt.legend(["middle class", "mid-high class", "high class"])
-# plt.plot(np.linspace(0, 5, 5), pricing_probabilities[1], 'r')
-# plt.plot(np.linspace(0, 5, 5), pricing_probabilities[2], 'b')
-# plt.plot(np.linspace(0, 5, 5), pricing_probabilities[3], 'g')
-# plt.show()
+plt.figure(0)
+plt.title("pricing_probabilities")
+plt.plot(np.linspace(200, 400, 5), pricing_probabilities[1], 'r')
+plt.plot(np.linspace(200, 400, 5), pricing_probabilities[2], 'b')
+plt.plot(np.linspace(200, 400, 5), pricing_probabilities[3], 'g')
+labels=[200,250,300,350,400]
+plt.xticks(np.linspace(200, 400, 5), labels)
+plt.legend(["middle class", "mid-high class", "high class"])
+plt.show()
 
 n_clicks_per_bid_by_class = {
     1: lambda x: np.sum(n_clicks_per_bid_by_feature[feature](x) for feature in features_from_class[1]),
@@ -46,13 +48,13 @@ n_clicks_per_bid_by_feature = {
     '10': lambda x: (1 - np.exp(-x * 2)) * 50 + 7,
     '11': lambda x: (1 - np.exp(-x * 2.5)) * 30 + 5,
 }
-# plt.figure(1)
-# plt.title("n_clicks_per_bid_functions")
-# plt.legend(["middle class", "mid-high class", "high class"])
-# plt.plot(bids, n_clicks_per_bid_functions[1](bids), 'r')
-# plt.plot(bids, n_clicks_per_bid_functions[2](bids), 'b')
-# plt.plot(bids, n_clicks_per_bid_functions[3](bids), 'g')
-# plt.show()
+plt.figure(1)
+plt.title("n_clicks_per_bid_functions")
+plt.plot(bids, n_clicks_per_bid_by_class[1](bids), 'r')
+plt.plot(bids, n_clicks_per_bid_by_class[2](bids), 'b')
+plt.plot(bids, n_clicks_per_bid_by_class[3](bids), 'g')
+plt.legend(["middle class", "mid-high class", "high class"])
+plt.show()
 n_clicks_per_bid_sigma = 3.0
 
 cum_cost_per_bid_by_class = {
@@ -66,28 +68,70 @@ cum_cost_per_bid_by_feature = {
     '10': lambda x: (1 - np.exp(-x * 2)) * 150 + 21,
     '11': lambda x: (1 - np.exp(-x * 2.5)) * 90 + 15,
 }
-# plt.figure(1)
-# plt.title("cumulative_cost_per_bid_functions")
-# plt.legend(["middle class", "mid-high class", "high class"])
-# plt.plot(bids, cumulative_cost_per_bid_functions[1](bids), 'r')
-# plt.plot(bids, cumulative_cost_per_bid_functions[2](bids), 'b')
-# plt.plot(bids, cumulative_cost_per_bid_functions[3](bids), 'g')
-# plt.show()
+plt.figure(1)
+plt.title("cumulative_cost_per_bid_functions")
+plt.plot(bids, cum_cost_per_bid_by_class[1](bids), 'r')
+plt.plot(bids, cum_cost_per_bid_by_class[2](bids), 'b')
+plt.plot(bids, cum_cost_per_bid_by_class[3](bids), 'g')
+plt.legend(["middle class", "mid-high class", "high class"])
+plt.show()
 cum_cost_per_bid_sigma = 10.0
 
 pricing_probabilities_by_phase = {
+    # 1: np.array([0.01, 0.99, 0.01, 0.01, 0.01]),
     1: np.array([0.05, 0.35, 0.15, 0.10, 0.13]),  # first phase
     # price*prob [2.5, 35, 22.5, 20, 32.5]
-    2: np.array([0.15, 0.19, 0.36, 0.22, 0.15]),  # second phase
-    # price*prob [7.5, 19, 54, 44, 37.5]
-    3: np.array([0.42, 0.18, 0.10, 0.08, 0.06]),  # third phase
-    # price*prob [21, 18, 15, 16, 15]
+    # 2: np.array([0.01, 0.01, 0.01, 0.01, 0.99]),
+    2: np.array([0.18, 0.20, 0.38, 0.22, 0.15]),  # second phase
+    #2: np.array([0.05, 0.35, 0.15, 0.10, 0.13]),
+    # price*prob [9, 20, 57, 44, 37.5]
+    # 3: np.array([0.01, 0.01, 0.99, 0.01, 0.01]),
+    3: np.array([0.28, 0.16, 0.12, 0.10, 0.06]),  # third phase
+    #3: np.array([0.05, 0.35, 0.15, 0.10, 0.13]),
+    # price*prob [14, 16, 18, 20, 15]
 }
+
+
+# pricing_probabilities_by_phase_6b = {
+#     # 1: np.array([0.01, 0.99, 0.01, 0.01, 0.01]),
+#     1: np.array([0.05, 0.35, 0.15, 0.10, 0.13]),  # first phase
+#     # price*prob [2.5, 35, 22.5, 20, 32.5]
+#     # 2: np.array([0.01, 0.01, 0.01, 0.01, 0.99]),
+#     2: np.array([0.45, 0.15, 0.65, 0.50, 0.43]),  # second phase
+#     # price*prob [9, 20, 57, 44, 37.5]
+#     # 3: np.array([0.01, 0.01, 0.99, 0.01, 0.01]),
+#     3: np.array([0.28, 0.16, 0.12, 0.10, 0.06]),  # third phase
+#     # price*prob [14, 16, 18, 20, 15]
+#     4: np.array([0.25, 0.35, 0.45, 0.05, 0.20]), #TODO cambiare 4 e 5
+#     5: np.array([0.30, 0.05, 0.15, 0.10, 0.40]),
+#
+# }
+
+pricing_probabilities_by_phase_6b = {
+    # 1: np.array([0.01, 0.99, 0.01, 0.01, 0.01]),
+    1: np.array([0.80, 0.10, 0.10, 0.10, 0.10]),  # first phase
+    # price*prob [2.5, 35, 22.5, 20, 32.5]
+    # 2: np.array([0.01, 0.01, 0.01, 0.01, 0.99]),
+    2: np.array([0.10, 0.80, 0.10, 0.10, 0.10]),  # second phase
+    # price*prob [9, 20, 57, 44, 37.5]
+    # 3: np.array([0.01, 0.01, 0.99, 0.01, 0.01]),
+    3: np.array([0.10, 0.10, 0.8, 0.10, 0.10]),  # third phase
+    # price*prob [14, 16, 18, 20, 15]
+    4: np.array([0.1, 0.1, 0.1, 0.80, 0.10]), #TODO cambiare 4 e 5
+    5: np.array([0.1, 0.1, 0.1, 0.10, 0.80]),
+
+}
+
+#EXP3 bid fixed so num of click fixed
+step6_fixed_bid = 2.79
+
+
+
 
 # plt.figure(0)
 # plt.title("pricing_probabilities step 5")
 # plt.legend(["normal period", "holiday period", "sales period"])
-# plt.plot(np.linspace(0, 5, 5), pricing_probabilities_by_phase[1] * (prices - cost), 'r')
-# plt.plot(np.linspace(0, 5, 5), pricing_probabilities_by_phase[2] * (prices - cost), 'b')
-# plt.plot(np.linspace(0, 5, 5), pricing_probabilities_by_phase[3] * (prices - cost), 'g')
+# plt.plot(np.linspace(0, 5, 5), pricing_probabilities_by_phase[1], 'r')
+# plt.plot(np.linspace(0, 5, 5), pricing_probabilities_by_phase[2], 'b')
+# plt.plot(np.linspace(0, 5, 5), pricing_probabilities_by_phase[3], 'g')
 # plt.show()
